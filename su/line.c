3111e2f9e75c50eeec43468fd053fed733ce0d61
Anterior <ccdevote@gmail.com>
2013-10-16 10:37:26 +0800

    define data line

---
+++ su/line.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "type.h"
+struct lineheader0
+{
+  u16 l1fld;   //L 1(num) F L D 
+  u16 len;
+  u32 llen;    // LLen
+};
+struct lineheader
+{
+  u16 l1fld;
+  u16 pad;
+  u32 offset;  //offset in current page
+};
+struct line
+{
+  struct lineheader *lh;  //line header
+  void *ld;               //line data
+};
+struct lineheader lharray[]; //total 1 + lharray[0].len
+
